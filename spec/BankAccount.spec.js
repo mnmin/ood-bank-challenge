e195fd17a1f98c48c9b98437e16591e1bb787ec5
Michele M <mnmin@gmail.com>
2022-06-16 17:44:43 +0100

    Domain Model + Set Up

---
+++ spec/BankAccount.spec.js
@@ -0,0 +1,42 @@
+const Transactions = require('../src/Transactions.js') 
+const BankAccount = require('../src/BankAccount.js')
+const Statements = require('../src/Statements.js')
+
+describe("BankAccount", () => {
+    let bankAccount
+
+    beforeEach(() => {
+        bankAccount = new BankAccount
+    })
+
+    it("", () => {
+    
+    
+      })
+})
+
+describe("Transactions", () => {
+    let transactions
+
+    beforeEach(() => {
+        transactions = new Transactions
+    })
+
+    it("", () => {
+    
+    
+    })
+})
+
+describe("Statements", () => {
+    let statements
+
+    beforeEach(() => {
+        statements = new Statements
+    })
+
+    it("", () => {
+    
+    
+    })
+})
---
+++ src/Statements.js
@@ -0,0 +1,10 @@
+const BankAccount = require('./BankAccount.js')
+const Transactions = require('./Transactions.js')
+
+class Statements {
+    construcotr() {
+
+    }
+}
+
+module.exports = Statements
